e71c68098434d69ab8bb57aae9041a64a69448ce
NuclearQuince <mihirsalian096@gmail.com>
2026-03-28 18:18:53 +0530

    fix: adds 2 blank lines to pass flake8 linting, part2

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,12 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json()['status'] == 'healthy'
+
 
 def test_health_check():
     response = client.get('/health')
